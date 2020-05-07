fe1c03a91d68d29d8194013ebc12193ae73f3258
fructuosowipf <beide@fructuosowipf.com>
2020-05-07 16:10:36 +0200

    start lesson 15

--- app.js
+++ app.js
@@ -1,37 +1,13 @@
-var one = new Vue({
-    el: '#vue-app-one',
+new Vue({
+    el: '#vue-app',
     data: {
-        title: 'Vue App One'
   
     },
     methods: {
       
     },
     computed: {
-        greet: function(){
-            return 'Hello from app one :-)!';
-        }
+
     }
 });
 
-var two = new Vue({
-    el: '#vue-app-two',
-    data: {
-        title: 'Vue App Two'
-  
-    },
-    methods: {
-        changeTitle: function(){
-            one.title = 'Crazy!';
-        }
-      
-    },
-    computed: {
-        greet: function(){
-            return 'Yo, this is app two speaking to you!!';
-        }
-        
-    }
-});
-
-two.title = "App Two Title Changed from outside";
