2f3e31c54772eadafe74680cb13e04e9a2a6243d
fructuosowipf <beide@fructuosowipf.com>
2020-05-07 16:36:34 +0200

    lesson 15 start

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
-new Vue({
-    el: '#vue-app',
+Vue.component('greeting',{
+    template: '<p>Hey there, I am a re-usable component</p>',
+    data: function(){
+        return {
+            name: 'Yoshi'
+        }
+    }
+})
+
+var one = new Vue({
+    el: '#vue-app-one',
     data: {
+
   
     },
     methods: {
       
     },
     computed: {
-
+ 
     }
 });
 
+var two = new Vue({
+    el: '#vue-app-two',
+    data: {
+        
+  
+    },
+    methods: {
+        
+      
+    },
+    computed: {
+        
+        
+    }
+});
+
+two.title = "App Two Title Changed from outside";
