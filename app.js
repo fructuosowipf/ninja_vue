38a28986d2e6aa54c22e5960b3dff7bde74d5edf
fructuosowipf <beide@fructuosowipf.com>
2020-05-07 11:29:03 +0200

    lesson 12 start

--- app.js
+++ app.js
@@ -102,4 +102,22 @@ new Vue({
     computed: {
 
     }
-})
+});
+
+new Vue({
+    el: '#v-for',
+    data: {
+        characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+        ninjas: [
+            { name: 'Ryu', age: 25 },
+            { name: 'Yoshi', age: 35 },
+            { name: 'Ken', age: 55 }
+        ]
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+});
